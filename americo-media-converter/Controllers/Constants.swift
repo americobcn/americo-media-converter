84151208a69666221417f599573848aa426c219a
americobcn <americo.cot@gmail.com>
2025-10-23 10:49:31 +0200

    WIP of ffprobe metadada

--- americo-media-converter/Controllers/Constants.swift
+++ americo-media-converter/Controllers/Constants.swift
@@ -8,6 +8,23 @@
 import Cocoa
 
 struct Constants {
+    let ffmpegSupportedExtensions: Set<String> = [
+        // Video formats
+        "mp4", "m4v", "mkv", "webm", "mov", "avi", "wmv", "flv", "f4v",
+        "swf", "mpg", "mpeg", "m2v", "3gp", "3g2", "mxf", "roq", "nsv",
+        "vob", "ogv", "drc", "gifv", "mng", "qt", "yuv", "rm", "rmvb",
+        "asf", "amv", "m4p", "mpv", "mp2", "mpe", "mpv2", "m2ts", "mts",
+        "ts", "divx", "dv", "gxf", "mj2", "mjpeg", "mjpg", "nut", "rv",
+        "wtv", "dvr-ms", "rec", "mod", "tod", "vro",
+        // Audio formats
+        "mp3", "aac", "wav", "flac", "ogg", "oga", "opus", "wma", "m4a",
+        "ac3", "eac3", "dts", "ape", "wv", "tta", "tak", "aiff", "aif",
+        "aifc", "au", "snd", "amr", "awb", "mp2", "mpa", "m4b", "m4r",
+        "3ga", "ra", "ram", "spx", "voc", "w64", "xa", "caf", "gsm",
+        "mlp", "mka", "shn", "vqf", "aa", "aa3", "aax", "act", "alac"
+    ]
+    
+    
     enum ConversionType {
         case audio
         case video
@@ -30,6 +47,7 @@ struct Constants {
         ]
     }
     
+    
     static func dropAlert(message: String, informative: String) -> Bool {
         let alert = NSAlert()
         alert.messageText = message
@@ -38,5 +56,5 @@ struct Constants {
         alert.addButton(withTitle: "OK")
         return alert.runModal() == .alertFirstButtonReturn
     }
-
+    
 }
--- americo-media-converter/Controllers/MediaController.swift
+++ americo-media-converter/Controllers/MediaController.swift
@@ -6,28 +6,177 @@
 //
 
 import Foundation
+import CoreMedia
 import AVFoundation
 
 
 class MediaController {
+    
     var urlAsset: AVURLAsset!
     var videoFormatDesc: CMFormatDescription!
     var audioFormatDesc: CMFormatDescription!
     var ffprobeURL: URL!
+    var ffmpegURL: URL!
     var format: [String:Any] = [:]
+    
+    // MARK: - FFprobe JSON Models
+
+    struct FFprobeOutput: Codable {
+        let streams: [FFprobeStream]
+        let format: FFprobeFormat
+    }
+
+    struct FFprobeStream: Codable {
+        let index: Int
+        let codecType: String
+        let codecName: String
+        let codecLongName: String?
+        let profile: String?
+        let codecTagString: String
+        let codecTag: String
+        let width: Int?
+        let height: Int?
+        let codedWidth: Int?
+        let codedHeight: Int?
+        let sampleAspectRatio: String?
+        let displayAspectRatio: String?
+        let pixFmt: String?
+        let level: Int?
+        let colorRange: String?
+        let colorSpace: String?
+        let colorTransfer: String?
+        let colorPrimaries: String?
+        let chromaLocation: String?
+        let fieldOrder: String?
+        let refs: Int?
+        let sampleFmt: String?
+        let sampleRate: String?
+        let channels: Int?
+        let channelLayout: String?
+        let bitsPerSample: Int?
+        let rFrameRate: String?
+        let avgFrameRate: String?
+        let timeBase: String?
+        let startPts: Int?
+        let startTime: String?
+        let duration: String?
+        let bitRate: String?
+        let nbFrames: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case index, profile, level, refs, duration
+            case codecType = "codec_type"
+            case codecName = "codec_name"
+            case codecLongName = "codec_long_name"
+            case codecTagString = "codec_tag_string"
+            case codecTag = "codec_tag"
+            case width, height
+            case codedWidth = "coded_width"
+            case codedHeight = "coded_height"
+            case sampleAspectRatio = "sample_aspect_ratio"
+            case displayAspectRatio = "display_aspect_ratio"
+            case pixFmt = "pix_fmt"
+            case colorRange = "color_range"
+            case colorSpace = "color_space"
+            case colorTransfer = "color_transfer"
+            case colorPrimaries = "color_primaries"
+            case chromaLocation = "chroma_location"
+            case fieldOrder = "field_order"
+            case sampleFmt = "sample_fmt"
+            case sampleRate = "sample_rate"
+            case channels
+            case channelLayout = "channel_layout"
+            case bitsPerSample = "bits_per_sample"
+            case rFrameRate = "r_frame_rate"
+            case avgFrameRate = "avg_frame_rate"
+            case timeBase = "time_base"
+            case startPts = "start_pts"
+            case startTime = "start_time"
+            case bitRate = "bit_rate"
+            case nbFrames = "nb_frames"
+        }
+    }
+
+    struct FFprobeFormat: Codable {
+        let filename: String
+        let nbStreams: Int
+        let formatName: String
+        let formatLongName: String?
+        let startTime: String?
+        let duration: String?
+        let size: String?
+        let bitRate: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case filename, duration, size
+            case nbStreams = "nb_streams"
+            case formatName = "format_name"
+            case formatLongName = "format_long_name"
+            case startTime = "start_time"
+            case bitRate = "bit_rate"
+        }
+    }
+
     
     init() {
         self.ffprobeURL = nil
         if !checkFFprobe() {
             _ = Constants.dropAlert(message: "ffprobe is missing", informative: "Some info will not be available until you install ffprobe binary in Resources folder of the app.\nIf ffprobe is located in /usr/local/bin/ffprobe, copy the binary in the Resources folder of the app.")
         }
+        
+        self.ffmpegURL = Bundle.main.url(forResource: "ffmpeg", withExtension: nil)
     }
     
     
     
     func isAVMediaType(url: URL) -> (isPlayable: Bool, formats: [String: Any]) {
-        let extensions: Set<String> = ["ogg", "weba", "webm", "eac3", "adx", "aea", "amr", "g722"]
-        if extensions.contains(url.pathExtension.lowercased()) {
+        let urlAsset = AVURLAsset(url: url)
+        if urlAsset.isPlayable {
+            format = getMetadata(asset: urlAsset)
+            // print("FORMAT: \(format)\n")
+            return (isPlayable: true, formats: format)
+        }
+        
+        
+        if Constants.ffmpegSupportedExtensions.contains(url.pathExtension.lowercased()) {
+            let task = Process()
+            let outputPipe = Pipe()
+            let errorPipe = Pipe()
+            task.standardOutput = outputPipe
+            task.standardError = errorPipe
+            
+            task.executableURL = ffmpegURL
+            task.arguments = ["-ss", "00:29:59", "-i", url.path, "-f", "null", "-"]
+            do {
+                try task.run()
+                task.waitUntilExit()
+            } catch {
+                // let error = String(decoding: task.standardError, as: UTF8.self)
+                print("Error")
+            }
+            
+            let status = task.terminationStatus
+            print("status: \(status)")
+            
+            // if status == 0 {
+            //     let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+            //     let output = String(decoding: outputData, as: UTF8.self)
+            //     print("output: \(output)")
+            //     print("Playable")
+            // } else {
+            //     print("Not Playable")
+            // }
+            
+            // print("Status: \(task.terminationStatus)")
+            //
+            
+            //
+            // let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+            // let error = String(decoding: errorData, as: UTF8.self)
+            // print("Error: \(error)")
+            
+        }
+/*
             let process = Process()
             process.executableURL = ffprobeURL
             let arguments = "-v quiet -show_format -show_streams -print_format json \(url.path)"  // ffprobe -v quiet -show_format -show_streams -print_format json input.mp4
@@ -36,35 +185,27 @@ class MediaController {
             let outputPipe = Pipe()
             process.standardOutput = outputPipe
             process.standardError = outputPipe
-                    
+            
             do {
                 try process.run()
                 process.waitUntilExit()
                 let data = outputPipe.fileHandleForReading.readDataToEndOfFile()
-                    do {
-                        if let dictionary = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                            // print("Swift Dict: \(dictionary)")
-                            format = dictionary
-                        }
+                do {
+                    if let dictionary = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        print("CMFD: \(dictionary)")
+                        return (isPlayable: true, formats: dictionary)
                     } catch {
                         print("Failed to parse JSON: $$error.localizedDescription)")
+                    } catch {
+                        print("Failed to run process: $$error)")
                     }
-
                 } catch {
                     print("Failed to run process: $$error)")
                 }
-            
-            //  TODO: Convert ffprobe json to CMFormatDescription
-            print("self.format: \(format)")
-            return (isPlayable: true, formats: format)
+            }
         }
+*/
         
-        let urlAsset = AVURLAsset(url: url)
-        if urlAsset.isPlayable {
-            format = getMetadata(asset: urlAsset)
-            // print("FORMAT: \(format)\n")
-            return (isPlayable: true, formats: format)
-        }
         
         return (isPlayable: false, formats: format)
     }
@@ -77,9 +218,7 @@ class MediaController {
         var timeFromatDesc: CMTimeCodeFormatDescription?
         
         var formats: [String:Any] = [:]
-        // print("ASSET DESCRIPTION: \(mediaAssetDescription(asset: asset, mediaType: kCMMediaType_Video))")
         for track in asset.tracks {
-            // print("DESCRIPTORS: \(track.formatDescriptions)")
             switch track.mediaType {
             case .video:
                 videoFormatDesc = ((track.formatDescriptions[0] ) as! CMVideoFormatDescription)
@@ -104,6 +243,7 @@ class MediaController {
     }
     
     
+    
     func mediaAssetDescription(asset: AVURLAsset, mediaType: CMMediaType) -> [Any] {
         let formatDescriptions = asset.tracks.flatMap { $0.formatDescriptions }
         // let mediaSubtypes = formatDescriptions
@@ -111,6 +251,7 @@ class MediaController {
         //     .map { CMFormatDescriptionGetMediaSubType($0 as! CMFormatDescription).toString() }
         return formatDescriptions.filter { CMFormatDescriptionGetMediaType($0 as! CMFormatDescription) == mediaType }
     }
+    
     
     
     func checkFFprobe() -> Bool {
@@ -141,6 +282,273 @@ class MediaController {
             }
         }
     }
+    
+    
+    
+    // MARK: - Conversion Functions
+
+    enum FormatDescriptionError: Error {
+        case unsupportedCodec(String)
+        case missingRequiredField(String)
+        case invalidData
+        case creationFailed
+    }
 
     
+    
+    // MARK: - Format Descriptions
+    
+    func createFormatDescriptions(from jsonData: Data) throws -> [CMFormatDescription] {
+        let decoder = JSONDecoder()
+        let ffprobeOutput = try decoder.decode(FFprobeOutput.self, from: jsonData)
+        
+        var formatDescriptions: [CMFormatDescription] = []
+        
+        for stream in ffprobeOutput.streams {
+            if let formatDesc = try? createFormatDescription(from: stream) {
+                formatDescriptions.append(formatDesc)
+            }
+        }
+        
+        return formatDescriptions
+    }
+
+    
+    
+    func createFormatDescription(from stream: FFprobeStream) throws -> CMFormatDescription {
+        switch stream.codecType {
+        case "video":
+            return try createVideoFormatDescription(from: stream)
+        case "audio":
+            return try createAudioFormatDescription(from: stream)
+        default:
+            throw FormatDescriptionError.unsupportedCodec(stream.codecType)
+        }
+    }
+
+    
+    
+    
+    // MARK: - Video Format Description
+
+    func createVideoFormatDescription(from stream: FFprobeStream) throws -> CMFormatDescription {
+        guard let width = stream.width, let height = stream.height else {
+            throw FormatDescriptionError.missingRequiredField("width or height")
+        }
+        
+        let codecType = fourCharCode(from: stream.codecName)
+        var formatDescription: CMFormatDescription?
+        
+        var extensions: [String: Any] = [:]
+        
+        // Add color information if available
+        if let colorPrimaries = stream.colorPrimaries {
+            extensions[kCVImageBufferColorPrimariesKey as String] = colorPrimaries
+        }
+        if let colorTransfer = stream.colorTransfer {
+            extensions[kCVImageBufferTransferFunctionKey as String] = colorTransfer
+        }
+        if let colorSpace = stream.colorSpace {
+            extensions[kCVImageBufferYCbCrMatrixKey as String] = colorSpace
+        }
+        
+        // Parse pixel aspect ratio
+        if let sarString = stream.sampleAspectRatio, sarString != "0:1" {
+            let components = sarString.split(separator: ":")
+            if components.count == 2,
+               let hSpacing = Int(components[0]),
+               let vSpacing = Int(components[1]) {
+                extensions[kCVImageBufferPixelAspectRatioKey as String] = [
+                    kCVImageBufferPixelAspectRatioHorizontalSpacingKey as String: hSpacing,
+                    kCVImageBufferPixelAspectRatioVerticalSpacingKey as String: vSpacing
+                ]
+            }
+        }
+        
+        let extensionDict = extensions.isEmpty ? nil : extensions as CFDictionary
+        
+        let status = CMVideoFormatDescriptionCreate(
+            allocator: kCFAllocatorDefault,
+            codecType: codecType,
+            width: Int32(width),
+            height: Int32(height),
+            extensions: extensionDict,
+            formatDescriptionOut: &formatDescription
+        )
+        
+        guard status == noErr, let formatDesc = formatDescription else {
+            throw FormatDescriptionError.creationFailed
+        }
+        
+        return formatDesc
+    }
+
+    
+    
+    // MARK: - Audio Format Description
+
+    func createAudioFormatDescription(from stream: FFprobeStream) throws -> CMFormatDescription {
+        guard let sampleRateString = stream.sampleRate,
+              let sampleRate = Double(sampleRateString) else {
+            throw FormatDescriptionError.missingRequiredField("sample_rate")
+        }
+        
+        let channels = UInt32(stream.channels ?? 2)
+        let codecType = fourCharCode(from: stream.codecName)
+        
+        var asbd = AudioStreamBasicDescription()
+        asbd.mSampleRate = sampleRate
+        asbd.mFormatID = codecType
+        asbd.mChannelsPerFrame = channels
+        
+        // Set additional properties based on codec
+        switch stream.codecName {
+        case "pcm_s16le", "pcm_s16be":
+            asbd.mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked
+            asbd.mBitsPerChannel = 16
+            asbd.mBytesPerFrame = 2 * channels
+            asbd.mFramesPerPacket = 1
+            asbd.mBytesPerPacket = asbd.mBytesPerFrame
+        case "pcm_s24le", "pcm_s24be":
+            asbd.mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked
+            asbd.mBitsPerChannel = 24
+            asbd.mBytesPerFrame = 3 * channels
+            asbd.mFramesPerPacket = 1
+            asbd.mBytesPerPacket = asbd.mBytesPerFrame
+        case "pcm_f32le", "pcm_f32be":
+            asbd.mFormatFlags = kAudioFormatFlagIsFloat | kAudioFormatFlagIsPacked
+            asbd.mBitsPerChannel = 32
+            asbd.mBytesPerFrame = 4 * channels
+            asbd.mFramesPerPacket = 1
+            asbd.mBytesPerPacket = asbd.mBytesPerFrame
+        case "aac":
+            asbd.mFramesPerPacket = 1024
+        default:
+            // Generic compressed format
+            asbd.mFramesPerPacket = 0
+        }
+        
+        var formatDescription: CMAudioFormatDescription?
+        let status = CMAudioFormatDescriptionCreate(
+            allocator: kCFAllocatorDefault,
+            asbd: &asbd,
+            layoutSize: 0,
+            layout: nil,
+            magicCookieSize: 0,
+            magicCookie: nil,
+            extensions: nil,
+            formatDescriptionOut: &formatDescription
+        )
+        
+        guard status == noErr, let formatDesc = formatDescription else {
+            throw FormatDescriptionError.creationFailed
+        }
+        
+        return formatDesc
+    }
+
+    // MARK: - Helper Functions
+
+    func fourCharCode(from codecName: String) -> CMVideoCodecType {
+        // Map common FFmpeg codec names to FourCC codes
+        switch codecName.lowercased() {
+        case "h264", "avc1":
+            return kCMVideoCodecType_H264
+        case "hevc", "h265":
+            return kCMVideoCodecType_HEVC
+//        case "vp8":
+//            return kCMVideoCodecType_VP8
+        case "vp9":
+            return kCMVideoCodecType_VP9
+        case "mpeg4":
+            return kCMVideoCodecType_MPEG4Video
+        case "mpeg2video":
+            return kCMVideoCodecType_MPEG2Video
+        case "mpeg1video":
+            return kCMVideoCodecType_MPEG1Video
+        case "mjpeg", "jpeg":
+            return kCMVideoCodecType_JPEG
+        case "prores":
+            return kCMVideoCodecType_AppleProRes422
+//        case "dnxhd":
+//            return CMVideoCodecType(kCMVideoCodecType_DVCPROHD)
+        case "aac":
+            return kAudioFormatMPEG4AAC
+        case "mp3":
+            return kAudioFormatMPEGLayer3
+        case "pcm_s16le", "pcm_s16be":
+            return kAudioFormatLinearPCM
+        case "pcm_f32le", "pcm_f32be":
+            return kAudioFormatLinearPCM
+        case "flac":
+            return kAudioFormatFLAC
+        case "alac":
+            return kAudioFormatAppleLossless
+        case "opus":
+            return kAudioFormatOpus
+//        case "vorbis":
+//            return kAudioFormatVorbis
+        default:
+            // Try to convert string to FourCC
+            let chars = Array(codecName.prefix(4).padding(toLength: 4, withPad: " ", startingAt: 0))
+            return FourCharCode(chars[0].asciiValue ?? 0) << 24 |
+                   FourCharCode(chars[1].asciiValue ?? 0) << 16 |
+                   FourCharCode(chars[2].asciiValue ?? 0) << 8 |
+                   FourCharCode(chars[3].asciiValue ?? 0)
+        }
+    }
+
+    
+    
+    func getFFprobeJSON(for url: URL) throws -> Data {
+        let process = Process()
+        process.executableURL = ffprobeURL
+        process.arguments = [
+            "-v", "quiet",
+            "-show_format",
+            "-show_streams",
+            "-print_format", "json",
+            url.path
+        ]
+        
+        let pipe = Pipe()
+        process.standardOutput = pipe
+        try process.run()
+        process.waitUntilExit()
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        return data
+    }
+
 }
+
+
+
+// MARK: - Usage Example
+/*
+func processFFprobeOutput(jsonData: Data) {
+    do {
+        let formatDescriptions = try createFormatDescriptions(from: jsonData)
+        
+        for (index, formatDesc) in formatDescriptions.enumerated() {
+            print("Stream \(index):")
+            
+            if CMFormatDescriptionGetMediaType(formatDesc) == kCMMediaType_Video {
+                let dimensions = CMVideoFormatDescriptionGetDimensions(formatDesc)
+                print("  Type: Video")
+                print("  Dimensions: \(dimensions.width)x\(dimensions.height)")
+                print("  Codec: \(CMFormatDescriptionGetMediaSubType(formatDesc).toString())")
+            } else if CMFormatDescriptionGetMediaType(formatDesc) == kCMMediaType_Audio {
+                if let asbd = CMAudioFormatDescriptionGetStreamBasicDescription(formatDesc) {
+                    print("  Type: Audio")
+                    print("  Sample Rate: \(asbd.pointee.mSampleRate) Hz")
+                    print("  Channels: \(asbd.pointee.mChannelsPerFrame)")
+                    print("  Codec: \(asbd.pointee.mFormatID.toString())")
+                }
+            }
+        }
+    } catch {
+        print("Error processing FFprobe output: \(error)")
+    }
+}
+*/
